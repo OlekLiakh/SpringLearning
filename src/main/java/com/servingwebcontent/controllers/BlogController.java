bab1c55a3e33e35e586b99bfa7678dda9996800c
Oleksandr Liakh <olekliakh1986@gmail.com>
2020-05-01 20:55:43 +0200

    Edit and delete post

--- src/main/java/com/servingwebcontent/controllers/BlogController.java
+++ src/main/java/com/servingwebcontent/controllers/BlogController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Controller
 public class BlogController {
 
@@ -32,6 +35,51 @@ public class BlogController {
                            @RequestParam String fullText, Model model){
         Post post = new Post(title, announcement, fullText);
         postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping ("/blog/{ID}")
+    public String blogDetails (@PathVariable(value = "ID") long ID, Model model){
+        if(!postRepository.existsById(ID)){
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(ID);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post",result);
+
+        return "blog-details";
+    }
+
+    @GetMapping("/blog/{ID}/edit")
+    public String postEdit (@PathVariable(value="ID") long ID, Model model){
+        if(!postRepository.existsById(ID)){
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(ID);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+
+        return "post-edit";
+    }
+
+    @PostMapping ("/blog/{ID}/edit")
+    public String postEdit (@PathVariable(value="ID") long ID, @RequestParam String title, @RequestParam String announcement,
+                           @RequestParam String fullText, Model model) {
+        Post post = postRepository.findById(ID).orElseThrow();
+        post.setTitle(title);
+        post.setAnnouncement(announcement);
+        post.setFullText(fullText);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+    @PostMapping ("/blog/{ID}/remove")
+    public String postDelete (@PathVariable(value="ID") long ID, Model model) {
+        Post post = postRepository.findById(ID).orElseThrow();
+            postRepository.delete(post);
         return "redirect:/blog";
     }
 
